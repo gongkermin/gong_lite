e938e9dc13f28c76dfbe8164920cd110229005dc
gongkermin <gongkrmin@icloud.com>
2024-12-19 09:14:44 +0800

    Add main.cpp and update .gitignore

---
+++ gd_extensions/Gong/src/register_types.h
@@ -0,0 +1,18 @@
+/* godot-cpp integration testing project.
+ *
+ * This is free and unencumbered software released into the public domain.
+ */
+
+#ifndef EXAMPLE_REGISTER_TYPES_H
+#define EXAMPLE_REGISTER_TYPES_H
+
+
+#include <gdextension_interface.h>
+#include <godot_cpp/core/class_db.hpp>
+
+
+using namespace godot;
+
+
+
+#endif // EXAMPLE_REGISTER_TYPES_H
